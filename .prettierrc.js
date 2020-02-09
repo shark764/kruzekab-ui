0040525d45d35a5412377fad27290cc1f04e93bd
Daniel Farid Hernández Cortez <farid.hdz.64@gmail.com>
2020-02-09 09:43:18 -0600

    Configuring prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,9 @@
 module.exports = {
-  bracketSpacing: false,
+  bracketSpacing: true,
   jsxBracketSameLine: true,
   singleQuote: true,
-  trailingComma: 'all',
+  trailingComma: 'none',
+  printWidth: 120,
+  arrowParens: 'avoid',
+  tabWidth: 2,
 };
