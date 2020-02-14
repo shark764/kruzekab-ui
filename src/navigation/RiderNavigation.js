3e9d116e0e32e19dc1a274e3e819653df47a38d0
Daniel Farid Hernández Cortez <farid.hdz.64@gmail.com>
2020-02-14 10:37:20 -0600

    Reestructuring navigation

--- src/navigation/RiderNavigation.js
+++ src/navigation/RiderNavigation.js
@@ -1,5 +1,6 @@
+import React, { Component } from 'react';
 import { createStackNavigator } from 'react-navigation-stack';
-import { createDrawerNavigator } from 'react-navigation-drawer';
+import { createDrawerNavigator, DrawerItems } from 'react-navigation-drawer';
 import Riders from '../screens/Rider/Groups/Riders';
 import NewRider from '../screens/Rider/Groups/Riders/New';
 import EditRider from '../screens/Rider/Groups/Riders/Edit';
@@ -11,6 +12,9 @@ import AppNavigation from './AppNavigation';
 import Home from '../screens/Rider/Home';
 import SelectAddress from '../screens/Rider/SelectAddress';
 import AddressDetails from '../screens/Rider/AddressDetails';
+import { SafeAreaView, View } from 'react-native';
+import { Button } from 'react-native-elements';
+import { HelpButton, HelpButtonText } from '../components/Form/Elements';
 
 const RiderStack = createStackNavigator(
   {
@@ -52,10 +56,33 @@ const RequestStack = createStackNavigator(
   }
 );
 
-const RiderNavigation = createDrawerNavigator({
-  'Request Ride': { screen: RequestStack },
-  'My Riders': { screen: RiderStack },
-  'My Groups': { screen: GroupStack }
-});
+const RiderNavigation = createDrawerNavigator(
+  {
+    'Request Ride': { screen: RequestStack },
+    'My Riders': { screen: RiderStack },
+    'My Groups': { screen: GroupStack }
+  },
+  {
+    contentComponent: props => (
+      <View>
+        <SafeAreaView forceInset={{ top: 'always', horizontal: 'never' }}>
+          <DrawerItems {...props} />
+
+          <HelpButton
+            onPress={() => props.navigation.navigate('Initial', { userType: null })}
+            titleStyle={{
+              color: '#5280e2',
+              textDecorationLine: 'underline'
+            }}>
+            <HelpButtonText>Sign out</HelpButtonText>
+          </HelpButton>
+        </SafeAreaView>
+      </View>
+    ),
+    drawerOpenRoute: 'DrawerOpen',
+    drawerCloseRoute: 'DrawerClose',
+    drawerToggleRoute: 'DrawerToggle'
+  }
+);
 
 export default RiderNavigation;
