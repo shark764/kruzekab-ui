1e2e23bd557be03a15b1892764020b3c52a8d9d1
Daniel Farid Hernández Cortez <farid.hdz.64@gmail.com>
2020-02-16 22:44:34 -0600

    Reestructuring navigation default options

--- src/screens/Rider/Groups/Riders/Edit/layout.js
+++ src/screens/Rider/Groups/Riders/Edit/layout.js
@@ -117,7 +117,7 @@ export default class EditRider extends Component {
     }
   };
 
-  handleChoosePhoto = hey => {
+  handleChoosePhoto = () => {
     const options = {
       noData: true
     };
